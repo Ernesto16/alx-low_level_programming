158c98ac45716a9859c11f818c031b6ab8d68f46
Ernesto16 <uzokwechitom@gmail.com>
2022-09-08 16:29:16 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
+
 /**
- * main- Serves as the entry point for the program
+ * main - print the string in the put function
  *
- * Return: By default returns zero if no erros otherwise returns non-zero value.
- *
- *  */
-
+ * Description: using the main function
+ * Return: By default returns zero if no erros otherwise returns non-zero value. */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-	        puts("\"Programming is like building a multilingual puzzle");
-
-		        return (0);
+	        return (0);
 
 }
