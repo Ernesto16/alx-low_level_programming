6d5c93f330dc8658c7f72e74cb17db867b9f95d5
Ernesto16 <uzokwechitom@gmail.com>
2022-09-21 14:24:37 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,8 +12,10 @@ void reverse_array(int *a, int n)
 int *start_c, *end_c, c;
 int i;
 
+
 start_c = a;
 end_c = a;
+
 
 for (i = 0; i < n - 1; i++)
 {
@@ -22,9 +24,11 @@ end_c++;
 
 
 
+
 for (i = 0; i < n / 2; i++)
 {
 
+	
 c = *end_c;
 *end_c = *start_c;
 *start_c = c;
